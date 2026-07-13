c2a8d1f186a49cb69e228beca8f69b5074599ea3
chila254 <franklinfinyange@gmail.com>
2026-07-13 12:37:02 +0000

    Remove pre-buffer delay from player, buffer fix will be at Kotlin level

--- lib/screens/m3u8_video_player_screen.dart
+++ lib/screens/m3u8_video_player_screen.dart
@@ -704,13 +704,6 @@ class _M3U8VideoPlayerScreenState extends State<M3U8VideoPlayerScreen> {
             : 'Switching to $selectedQuality...',
       );
       await controller.initialize();
-
-      // Pre-buffer: wait for enough data before playing to avoid mid-playback pauses
-      if (shouldPlay && isHls) {
-        _showStatus('Buffering...');
-        await Future.delayed(const Duration(seconds: 2));
-      }
-
       if (position > Duration.zero) await controller.seekTo(position);
       if (shouldPlay) await controller.play();
 
