84b4ac7cf07edbe3410d5ca2521989213d1a608e
Franklin Chilango <franklinfinyange@gmail.com>
2026-07-18 13:44:34 +0300

    Bump versionCode to 4 and versionName to 1.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.maxstream.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
     }
 
     signingConfigs {
