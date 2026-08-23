4d4eced063f5d113cd49933b45ca6da60fb1e775
chila254 <franklinfinyange@gmail.com>
2026-08-23 00:07:08 +0000

    fix: add missing kotlinx.coroutines.cancel import

--- android/app/src/main/kotlin/com/maxstream/app/StreamExtractor.kt
+++ android/app/src/main/kotlin/com/maxstream/app/StreamExtractor.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
