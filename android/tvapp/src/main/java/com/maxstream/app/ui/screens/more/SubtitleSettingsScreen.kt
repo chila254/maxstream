4870b9661fe74ae5c13e7d686930c8fe1ab41bdd
chila254 <franklinfinyange@gmail.com>
2026-09-07 09:07:03 +0000

    Fix missing imports: toArgb (SubtitleSettingsScreen) and focusRequester (UpdatesScreen)

--- android/tvapp/src/main/java/com/maxstream/app/ui/screens/more/SubtitleSettingsScreen.kt
+++ android/tvapp/src/main/java/com/maxstream/app/ui/screens/more/SubtitleSettingsScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
